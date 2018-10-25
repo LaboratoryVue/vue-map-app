0cb9465372568ab0808a715a8597347a2d29c39e
Valery Semenenko <gearmobile@gmail.com>
2018-10-25 15:03:07 +0300

    vue-map

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+a {
+  text-decoration: none;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="home">
+    <div class="home__map"></div>
+    <div class="home__nav">
+      <nav class="navigation">
+        <a class="navigation__item" href="#">moscow</a>
+        <a class="navigation__item" href="#">petersberg</a>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .home {
+    display: flex;
+    // width: 1280px;
+    width: 100%;
+    height: 720px;
+    margin: 0 auto;
+
+    &__map {
+      width: 920px;
+      height: 100%;
+      background-color: black;
+    }
+
+    &__nav {
+      width: 360px;
+      height: 100%;
+      background-color: rgba(255,0,0, 0.1);
+    }
+
+    & .navigation {
+      display: flex;
+      flex-direction: column;
+      padding: 60px 30px;
+
+      &__item {
+        text-transform: uppercase;
+        color: black;
+        margin-bottom: 1rem;
+
+        &:last-child {
+          margin-bottom: 0;
+        }
+
+        &:hover,
+        &:active {
+          color: green;
+        }
+      }
+    }
+  }
+</style>
